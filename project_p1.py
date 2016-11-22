687d5a297bfe215d19195f3bfa097d33fbb9dd64
Andrew Stier <andrew@cerebri.com>
2016-11-21 23:33:51 -0600

    Added boilerplate code for descent problems. Added gitignore files

--- project_p1.py
+++ project_p1.py
@@ -3,7 +3,19 @@ Written by: Andrew Stier, Farzan Memarian, Sid Desai'''
 
 from __future__ import division
 import numpy as np
+import numpy.random as rn
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+import math
+import sys
+from scipy import linalg as LA
+import math
 
+def bfgs(x,J):
+	return x
+
+def obfgs(x,J):
+	return x
 
 def make_J_matrix(n):
 	J = np.zeros((n,n))
@@ -15,10 +27,41 @@ def make_J_matrix(n):
 
 	return J
 
+def descent(update, J, x_start, x_star, T=100):
+    x = x_start
+    error = []
+    for t in xrange(T):
+        
+        x = update(x, J)
+
+        if (t % 1 == 0) or (t == T - 1):
+            #calculate the error of this iteration
+            error.append(la.norm(x - x_star)**2)
+
+            assert not np.isnan(error[-1])
+
+    return x, error
+
 def main():
 	n = 5
 	J = make_J_matrix(n)
-	print J
+	
+	x_star = np.ones((5,1))
+	x_start = np.ones((5,1))
+   
+	x, errors_1 = descent(bfgs, J, x_start, x_star, T=20)
+	x, errors_2 = descent(obfgs, J, x_start, x_star, T=20)
+
+
+	# plot results
+	plt.clf()
+	plt.plot(errors_1, label="BFGS")
+	plt.plot(errors_2, label="oBFGS")
+	plt.title('Error')
+	plt.legend()
+	plt.show()
+	plt.savefig('Problem1.eps')
+
 
 
 if __name__ == "__main__":
